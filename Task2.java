0321f898b9aa017c05a8824e9075b8ecc92c1c0a
onecommitman <nurimanov102@gmail.com>
2021-12-27 17:53:09 +0500

    Добавил REGEX, вывод слов в алфавитном порядке

--- Task2.java
+++ Task2.java
@@ -1,16 +1,24 @@
 package IBS_Tasks;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task2 {
     public static void main(String[] args) throws IOException{
         StringBuffer fileData = new StringBuffer();
         BufferedReader reader = new BufferedReader(
-                new FileReader("G:\\Doc\\Text.txt"));
+                new FileReader("D:\\Doc\\Text.txt"));
         char[] buf = new char[1024];
         int numRead=0;
         while((numRead=reader.read(buf)) != -1){
@@ -19,6 +27,17 @@ public class Task2 {
         }
 
         String text = new String(fileData);
-        System.out.println(text);
+        List<String> list = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[А-Яа-яёЁ0-9]+");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()){
+            //System.out.println(matcher.group());
+            list.add(matcher.group().toLowerCase());
+        }
+        Collections.sort(list);
+
+        for(String s:list){
+            System.out.println(s);
+        }
     }
 }
