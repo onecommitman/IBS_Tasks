b1a5b2e53d27bddea7b84b3208f9b2770bfef63b
onecommitman <nurimanov102@gmail.com>
2021-12-31 16:55:32 +0500

    Задача решена. Файл открывается по абсолютному и относительному пути.

--- Task2.java
+++ Task2.java
@@ -1,7 +1,7 @@
 package IBS_Tasks;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -10,45 +10,51 @@ import java.util.regex.Pattern;
 
 public class Task2 {
     public static void main(String[] args){
+        File file = new File(args[0]);
         StringBuffer fileData = new StringBuffer();
-        //BufferedReader reader = new BufferedReader(new FileReader("D:\\Doc\\Text.txt"));
-        try(BufferedReader reader = new BufferedReader(new FileReader("Text.txt"));){
-            char[] buf = new char[1024];
-            int numRead=0;
-            while((numRead=reader.read(buf)) != -1){
-                String readData = String.valueOf(buf, 0, numRead);
-                fileData.append(readData);
+        if(file.exists()) {
+            System.out.println("Открыт файл " + file.getName());
+            System.out.println("Список слов в файле и количество повторений: ");
+            try(BufferedReader reader = new BufferedReader(new FileReader(args[0]))){
+
+
+                char[] buf = new char[1024];
+                int numRead=0;
+                while((numRead=reader.read(buf)) != -1){
+                    String readData = String.valueOf(buf, 0, numRead);
+                    fileData.append(readData);
+                }
+
+                String text = new String(fileData);
+                List<String> words = new ArrayList<>();
+                Pattern pattern = Pattern.compile("[A-Za-zА-Яа-яёЁ0-9]+");
+                Matcher matcher = pattern.matcher(text);
+                while (matcher.find()){
+                    words.add(matcher.group().toLowerCase());
+                }
+
+                Collections.sort(words);
+
+                Map<String, Integer> dictionary = new HashMap<>();
+                for (String word: words) {
+                    Integer frequency = dictionary.get(word);
+                    dictionary.put(word, frequency == null ? 1 : frequency + 1);
+                }
+
+                //Выводим результат
+                dictionary.forEach((key, value) -> System.out.println(key + " " + value));
+                int maxValueInMap=(Collections.max(dictionary.values())); //поиск максимального повторения
+                System.out.println("\nЭти слова повторяются больше всех (" + maxValueInMap + " раз(а)):");
+                for(Map.Entry<String, Integer> item : dictionary.entrySet()){
+                    if (item.getValue()==maxValueInMap) {
+                        System.out.println(item.getKey());     // вывож слов(а) с максимальным повторением
+                    }
+                }
             }
-        }
-        catch (IOException e){
-            System.out.println(e.getMessage());
-        }
 
-
-        String text = new String(fileData);
-        List<String> words = new ArrayList<>();
-        Pattern pattern = Pattern.compile("[А-Яа-яёЁ0-9]+");
-        Matcher matcher = pattern.matcher(text);
-        while (matcher.find()){
-            //System.out.println(matcher.group());
-            words.add(matcher.group().toLowerCase());
-        }
-        Collections.sort(words);
-
-        Map<String, Integer> dictionary = new HashMap<>();
-        for (String word: words) {
-            Integer frequency = dictionary.get(word);
-            dictionary.put(word, frequency == null ? 1 : frequency + 1);
-        }
-
-        //Выводим результат
-        dictionary.forEach((key, value) -> System.out.println(key + " " + value));
-
-        int maxValueInMap=(Collections.max(dictionary.values())); //поиск максимального повторения
-        for(Map.Entry<String, Integer> item : dictionary.entrySet()){
-            if (item.getValue()==maxValueInMap) {
-                System.out.println("Самое повторяющееся слово: " + item.getKey());     // вывож слов(а) с максимальным повторением
+            catch (IOException e){
+                System.out.println(e.getMessage());
             }
-        }
+        } else System.out.println("Указанного файла не существует!");
     }
 }
